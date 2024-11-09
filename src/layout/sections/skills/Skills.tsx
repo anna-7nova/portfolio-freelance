818a3f591f7a1ce06d03242c07152c1ae47ed915
anna-7nova <anna.7nova@yandex.by>
2024-11-10 02:04:20 +0300

    add the structure of section skills

---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { StyledSectionTitle } from '../../../components/StyledSectionTitle';
+import { Skill } from './skill/Skill';
+
+
+
+export const Skills = () => {
+    return (
+        <StyledSkills>
+            <StyledSectionTitle>My Skills</StyledSectionTitle>
+            <FlexWrapper wrap='wrap' justify='space-between'>
+            <Skill IconSkillId="code" titleValue="html5" textValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"/>
+            <Skill IconSkillId="css" titleValue="css3" textValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"/>
+            <Skill IconSkillId="react" titleValue="React" textValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"/>
+            <Skill IconSkillId="typescript" titleValue="typescript" textValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"/>
+            <Skill IconSkillId="styledComponents" titleValue="styled components" textValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"/>
+            <Skill IconSkillId="figma" titleValue="web design" textValue="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"/>
+            </FlexWrapper>
+        </StyledSkills>
+    );
+};
+
+const StyledSkills = styled.section`
+background-color: #7fe5c0;
+text-align: center;
+`
---
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Icon } from '../../../../components/icon/Icon';
+import styled from 'styled-components';
+
+
+type SkillPropsType = {
+    IconSkillId: string
+    titleValue: string
+    textValue: string
+}
+
+export const Skill = (props: SkillPropsType) => {
+    return (
+        <StyledSkill>
+            <Icon iconId={props.IconSkillId}/>
+            <SkillTitle>{props.titleValue}</SkillTitle>
+            <SkillText>{props.textValue}</SkillText>
+        </StyledSkill>
+    );
+}
+
+const StyledSkill = styled.div`
+width: 30%;
+background-color: #ffffff;
+margin: 10px;
+`
+
+const SkillTitle = styled.h3`
+
+`
+const SkillText = styled.p`
+
+`
