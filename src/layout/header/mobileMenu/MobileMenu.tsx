543ad6dbb6d0d43f66e23b4a13bc83b2a49a31f9
anna-7nova <anna.7nova@yandex.by>
2024-11-24 00:13:37 +0300

    update adaptive for section header

--- src/layout/header/mobileMenu/MobileMenu.tsx
+++ src/layout/header/mobileMenu/MobileMenu.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { theme } from '../../../styles/Theme';
-import { isPromise } from 'util/types';
 
 
 export const MobileMenu = (props: { menuItems: Array<string> }) => {
@@ -30,29 +29,34 @@ export const MobileMenu = (props: { menuItems: Array<string> }) => {
 };
 
 const StyledMobileMenu = styled.nav`
-    
+    display: none;
+
+    @media ${theme.media.tablet} {
+    display: block;
+}  
 `
 
-const BurgerButton = styled.div<{isOpen: boolean}>`
+const BurgerButton = styled.div<{ isOpen: boolean }>`
     position: fixed;
-    width: 200px;
-    height: 200px;
-    top: -100px;
-    right: -100px;
+    width: 100px;
+    height: 100px;
+    top: 0;
+    right: 0;
+    z-index: 999999;
 
 
     span {
-        display: block;
+        display: inline-block;
         width: 36px;
         height: 2px;
-        color: ${theme.colors.font};
+        background-color: ${theme.colors.font};
         
         position: absolute;
         left: 40px;
         bottom: 50px;
 
-        ${props => props.isOpen && css<{isOpen: boolean}>`
-        color: #ffffff0;
+        ${props => props.isOpen && css<{ isOpen: boolean }>`
+        background-color: rgba(255, 255, 255, 0);
         `}
 
         
@@ -61,37 +65,37 @@ const BurgerButton = styled.div<{isOpen: boolean}>`
             content: "";
             display: inline-block;
             transform: translateY(-10px);
-            width: 100%;
+            width: 36px;
             height: 2px;
-            color: ${theme.colors.font};
+            background-color: ${theme.colors.font};
 
             position: absolute;
-        }
 
-        ${props => props.isOpen && css<{isOpen: boolean}>`
-        transform: rotate(-45deg) translateY(0);
+            ${props => props.isOpen && css<{ isOpen: boolean }>`
+            transform: rotate(-45deg) translateY(0);
         `}
+        }
 
         &::after{
             content: "";
             display: inline-block;
             transform: translateY(10px);
-            width: 75%;
+            width: 27px;
             height: 2px;
-            color: ${theme.colors.font};
+            background-color: ${theme.colors.font};
 
             position: absolute;
             right: 0;
-        }
 
-        ${props => props.isOpen && css<{isOpen: boolean}>`
-        transform: rotate(45deg) translateY(0);
-        width: 100%;
+            ${props => props.isOpen && css<{ isOpen: boolean }>`
+            transform: rotate(45deg) translateY(0);
+            width: 36px;
         `}
+        }
     }
 `
 
-const MobileMenuPopup = styled.div<{isOpen: boolean}>`
+const MobileMenuPopup = styled.div<{ isOpen: boolean }>`
     position: fixed;
     top: 0;
     right: 0;
@@ -101,7 +105,7 @@ const MobileMenuPopup = styled.div<{isOpen: boolean}>`
     background-color: rgba(31, 31, 32, 0.9);
     display: none;
 
-    ${props => props.isOpen && css<{isOpen: boolean}>`
+    ${props => props.isOpen && css<{ isOpen: boolean }>`
         display: flex;
         justify-content: center;
         align-items: center;   
